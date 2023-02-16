6501df59e6a6d86da3c92e4eba64f3398acab535
aaryamann171 <aaryamann171@gmail.com>
2023-02-17 02:47:04 +0530

    added rockets page, rocket detail page

---
+++ src/pages/rockets/index.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Rockets = ({ rocketsData }) => {
+    return (
+        <div>
+            <div className="m-8 p-10">
+                <h1 className="text-4xl font-bold">SpaceX Rockets</h1>
+            </div>
+            {
+                rocketsData.map((rocket, index) => {
+                    return (
+                        <Link href={`/rockets/${rocket.id}`} key={index}>
+                            <div className="mb-10 bg-foreground rounded shadow m-8 p-10 flex">
+                                <div className="w-1/4">
+                                    <h1 className="text-3xl font-bold mb-4">{rocket.name}</h1>
+                                    <Image src={`${rocket.flickr_images[0]}`} width={200} height={200}></Image>
+                                </div>
+                                <div className="w-3/4 m-auto">
+                                    <p className="text-lg mt-4 text-slate-800">{rocket.description}</p>
+                                </div>
+                            </div>
+                        </Link>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export const getStaticProps = async (context) => {
+    const rocketsEndpoints = 'https://api.spacexdata.com/v4/rockets';
+    const rocketsRes = await fetch(rocketsEndpoints);
+    const rocketsResJSON = await rocketsRes.json();
+    return {
+        props: {
+            rocketsData: rocketsResJSON
+        },
+    }
+}
+
+export default Rockets; 
