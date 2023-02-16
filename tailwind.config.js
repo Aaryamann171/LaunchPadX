13c7f00c03b0770e726c8d6e16276f25b6592c3b
aaryamann171 <aaryamann171@gmail.com>
2023-02-16 23:23:29 +0530

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: (theme) => ({
+        primary: "var(--primary)",
+        accent: "var(--accent)",
+        foreground: "var(--foreground)"
+      }),
+    },
   },
   plugins: [],
 }
+
