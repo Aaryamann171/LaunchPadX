f9139098fabbe230cd6033fdb37a463fde9e8807
aaryamann171 <aaryamann171@gmail.com>
2023-02-22 23:49:07 +0530

    updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -32,29 +32,28 @@ const About = () => {
             />
             <div className="mb-10 bg-foreground dark:bg-foregroundDark rounded shadow m-4 md:m-8 p-4 md:p-10 border-t border-l border-b-8 border-r-8 border-black">
                 <div className="mb-4">
-                <h3 className="text-xl text-zinc-700 dark:text-zinc-400"> 
-                LaunchPadX is a simple SpaceX dashboard built using Next.js and TailwindCSS. All the data is fetched from SpaceX public APIs.
-                </h3>
+                    <h3 className="text-xl text-zinc-700 dark:text-zinc-400"> 
+                    The LaunchPax Dashboard is a web application that provides real-time information about SpaceX launches and missions. 
+                    The platform is built using <span className="font-mono">Next.js</span> and <span className="font-mono">TailwindCSS</span>, 
+                    and utilizes the SpaceX API to fetch and display the latest launch data, as well as some information about various Space X rockets and The Starman. 
+                    Users can explore past and upcoming launches, view detailed mission information, and keep up-to-date with the latest developments from SpaceX. 
+                    The aim of this project is to provide a user-friendly and informative platform for space enthusiasts and casual observers alike, to make it easy for anyone to follow the latest SpaceX news and developments.
+                    </h3>
                 </div>
-                <h3 className="text-xl text-zinc-700 dark:text-zinc-400 mb-4"> 
-                It showcases various missions and launches SpaceX has undertaken. There is also a dedicated page for SpaceX rockets. 
-                The design is still a work in progress. I just hacked it together in a couple of days while exploring the SpaceX API.
-                </h3>
-                <div>
-            </div>
             </div>
             <SectionHeader
                 icon={<TfiInfoAlt/>}
                 heading="About Me"
             />
             <div className="mb-10 bg-foreground dark:bg-foregroundDark rounded shadow m-4 md:m-8 p-2 md:p-10 flex flex-col md:flex-row border-t border-l border-b-8 border-r-8 border-black">
-            <div className="md:w-1/3 bg-white dark:bg-black m-4 md:p-8 rounded text-center border-t border-l border-b-8 border-r-8 border-black">
+            <div className="md:w-1/3 bg-white dark:bg-black m-4 md:p-8 rounded text-center border-2 border-black">
                 <Image src={'/me.png'} width={500} height={500} className="m-auto" alt="me"></Image>
             </div>
             <div className="md:w-2/3 p-4 m-auto">
                 <p className="text-xl md:text-2xl mt-4 text-zinc-800 dark:text-zinc-300 md:p-10">
-                    I like to play around with various technologies and collaborate with like minded people. 
-                    I am currently working as a Full Stack Developer. I work with Django, Flask and Next.js.
+                    I am an experienced Full Stack Developer with 2 years of professional experience in the field. 
+                    I am proficient in a range of technologies including Django, Flask, and Next.js. 
+                    I have a passion for exploring and experimenting with new technologies and am always looking to collaborate with like-minded individuals on challenging and innovative projects.
                 </p>
                 <div className="mt-8 flex flex-col md:flex-row text-center justify-evenly">
                     <SocialBtn 
